565c7e2985504ba354733cf0109f2e19d19ea089
C23069362 <georged8@cardiff.ac.uk>
2024-12-03 20:32:54 -0800

    Amended the algorithm so that it finishes when the nodes path reaches the goal node. Also added some more robust error and exception handling, should the program fail.

--- RRT_Algorithm.py
+++ RRT_Algorithm.py
@@ -1,6 +1,7 @@
 import pygame
 from RRTbasePy import RRTGraph
 from RRTbasePy import RRTMap
+import time
 
 def main():
     dimensions = (600, 1000)
@@ -9,6 +10,7 @@ def main():
     obsdim = 30
     obsnum = 50
     iterations = 0
+    initialtime = 0
 
     pygame.init()
     map = RRTMap(start, goal, dimensions, obsdim, obsnum)
@@ -17,20 +19,13 @@ def main():
     obstacles = graph.makeobs()
     map.drawMap(obstacles)
 
-        # x, y = graph.sample_envir()
-        # n = graph.number_of_nodes()
-        # graph.add_node(n, x, y)
-        # graph.add_edge(n-1, n)
-        # x1, y1 = graph.x[n], graph.y[n]
-        # x2, y2 = graph.x[n-1], graph.y[n-1]
-        # if (graph.isFree()):
-        #     pygame.draw.circle(map.map, map.Red, (graph.x[n], graph.y[n]), map.nodeRad, map.nodeThickness)
-        #     if not graph.crossObstacle(x1, x2, y1, y2):
-        #         pygame.draw.line(map.map, map.Blue, (x1, y1), (x2, y2), map.edgeThickness)
-        #
-        # pygame.display.update()
+    initialtime = time.time()
+    while not graph.path_to_goal():
+        elapsedTime = time.time() - initialtime
+        initialtime = time.time()
+        if elapsedTime > 10:
+            raise Exception("Elapsed Time limit exceeded")
 
-    while (iterations < 500):
         if iterations % 10 == 0:
             X, Y, Parent = graph.bias(goal)
             pygame.draw.circle(map.map, map.grey, (X[-1], Y[-1]), map.nodeRad+2, 0)
@@ -44,9 +39,16 @@ def main():
             pygame.display.update()
         iterations += 1
 
+    map.drawPath(graph.getPathCoords())
     pygame.display.update()
     pygame.event.clear()
     pygame.event.wait(0)
 
 if __name__ == '__main__':
-    main()
+    result = False
+    while not result:
+        try:
+            main()
+            result = True
+        except:
+            result = False
--- RRTbasePy.py
+++ RRTbasePy.py
@@ -34,8 +34,9 @@ class RRTMap:
         pygame.draw.circle(self.map, self.Green, self.goal, self.nodeRad+20, 1)
         self.drawObs(obstacles)
 
-    def drawPath(self):
-        pass
+    def drawPath(self, path):
+        for node in path:
+            pygame.draw.circle(self.map, self.Red, node, self.nodeRad+3, 0)
 
     def drawObs(self, obstacles):
         obstaclesList = obstacles.copy()
@@ -180,10 +181,22 @@ class RRTGraph:
                 self.add_node(nrand, x, y)
 
     def path_to_goal(self):
-        pass
+        if self.goalFlag:
+            self.path = []
+            self.path.append(self.goalState)
+            newpos = self.parent[self.goalState]
+            while (newpos != 0):
+                self.path.append(newpos)
+                newpos = self.parent[newpos]
+            self.path.append(0)
+        return self.goalFlag
 
     def getPathCoords(self):
-        pass
+        pathCoords = []
+        for node in self.path:
+            x, y = self.x[node], self.y[node]
+            pathCoords.append((x, y))
+        return pathCoords
 
     def bias(self, ngoal):
         n = self.number_of_nodes()
